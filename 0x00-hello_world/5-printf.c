51ddd6609509043b67cd244a12994b6c222b4825
kurolinks <wsubiri@gmail.com>
2022-09-08 21:17:06 -0700

    edited gile 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print the sting in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programmins is like building a mulrilingual puzzle
+ * Rettun: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
