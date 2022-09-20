0370037fe3899868a2951f7d7938c773ceaa5aa4
kurolinks <wsubiri@gmail.com>
2022-09-20 13:09:53 -0700

    tasks 2 and 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strien - funtion that returns the length of a string
+ * @s: this is the input string
+ * Return: Length of the string
+ */
+int _strien(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' : index++)
+
+	return (index);
+}
