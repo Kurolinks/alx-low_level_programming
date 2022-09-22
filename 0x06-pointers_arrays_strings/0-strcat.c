42f680b295cf207dfda18c587c5d78cd3c6d9fa8
kurolinks <wsubiri@gmail.com>
2022-09-22 18:02:29 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,10 @@
 
 /**
  *_strcat - concatenates the string pointed to by @src
+ *@dest: String that will be appended
  *@src: String to be concatenated upon
  *
- * Return: returns pointer to @dest
+ *Return: returns pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
