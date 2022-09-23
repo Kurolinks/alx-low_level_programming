1617cfc319c85e2e87dab03346e004e89af0c409
kurolinks <wsubiri@gmail.com>
2022-09-23 04:30:06 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,6 +7,7 @@
  */
 
 char *cap_string(char *s)
+
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
@@ -14,10 +15,8 @@ char *cap_string(char *s)
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
-
 	while (*(s + count) != '\0')
 	{
-
 		for (i = 0; i < 13; i++)
 		{
 			if (*(s + count) == sep_words[i])
@@ -31,3 +30,5 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
+
+
